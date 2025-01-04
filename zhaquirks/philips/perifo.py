21a94a700ec20af24ad27244caa8b8a3ab821170
Krzysztof Jagiello <me@kjagiello.com>
2025-01-04 19:36:00 +0100

    Add Hue Perifo

---
+++ zhaquirks/philips/perifo.py
@@ -0,0 +1,19 @@
+"""Hue Perifo device."""
+
+from zigpy.quirks.v2 import QuirkBuilder
+
+from zhaquirks.philips.gradient import PhilipsGradientCluster
+
+(
+    QuirkBuilder()
+    .applies_to("Signify Netherlands B.V.", "929003116301")
+    .applies_to("Signify Netherlands B.V.", "929003116401")
+    .applies_to("Signify Netherlands B.V.", "929003116501")
+    .applies_to("Signify Netherlands B.V.", "929003116601")
+    .friendly_name(
+        model="Hue Perifo Light Tube",
+        manufacturer="Philips",
+    )
+    .replaces(PhilipsGradientCluster, endpoint_id=11)
+    .add_to_registry()
+)
