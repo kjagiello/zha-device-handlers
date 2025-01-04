52ab3acc90111f19bec6ef5cd8a4ccc23015dd05
Krzysztof Jagiello <me@kjagiello.com>
2025-01-04 19:32:05 +0100

    Add Hue tubes

---
+++ zhaquirks/philips/tube.py
@@ -0,0 +1,21 @@
+"""Hue Play Gradient Light Tube device."""
+
+from zigpy.quirks.v2 import QuirkBuilder
+
+from zhaquirks.philips.gradient import PhilipsGradientCluster
+
+(
+    QuirkBuilder()
+    .applies_to("Signify Netherlands B.V.", "915005987901")
+    .applies_to("Signify Netherlands B.V.", "915005988001")
+    .applies_to("Signify Netherlands B.V.", "915005988101")
+    .applies_to("Signify Netherlands B.V.", "915005988201")
+    .applies_to("Signify Netherlands B.V.", "915005988401")
+    .applies_to("Signify Netherlands B.V.", "915005988501")
+    .friendly_name(
+        model="Hue Play Gradient Light Tube",
+        manufacturer="Philips",
+    )
+    .replaces(PhilipsGradientCluster, endpoint_id=11)
+    .add_to_registry()
+)
